2716ee9278aa849a5e51d799da00c2d7f43f9b3c
yuhsuankao <a00000000001.tw@gmail.com>
2019-08-30 10:40:32 +0800

    gg added

--- R/iNEXT_sar_inc_FUN.R
+++ R/iNEXT_sar_inc_FUN.R
@@ -1,5 +1,3 @@
-library(reshape2)
-
 H_hat_inc_wor = function(y, T_star){
 
   nT = y[1]
@@ -440,6 +438,255 @@ BootstrapFun = function (x, FunName, datatype, B) {
     BootstrapFun.ince(y = x, FunName, datatype, B)
   }
 }
+DataInfo = function (x, datatype = "abundance") {
+  TYPE <- c("abundance", "incidence", "incidence_freq", "incidence_raw")
+  if (is.na(pmatch(datatype, TYPE)))
+    stop("invalid datatype")
+  if (pmatch(datatype, TYPE) == -1)
+    stop("ambiguous datatype")
+  datatype <- match.arg(datatype, TYPE)
+  if (datatype == "incidence_freq")
+    datatype <- "incidence"
+  if (datatype == "incidence_raw") {
+    if (class(x) == "list") {
+      x <- lapply(x, as.incfreq)
+    }
+    else {
+      x <- as.incfreq(x)
+    }
+    datatype <- "incidence"
+  }
+  Fun.abun <- function(x) {
+    n <- sum(x)
+    fk <- sapply(1:10, function(k) sum(x == k))
+    f1 <- fk[1]
+    f2 <- fk[2]
+    Sobs <- sum(x > 0)
+    f0.hat <- ifelse(f2 == 0, (n - 1)/n * f1 * (f1 - 1)/2,
+                     (n - 1)/n * f1^2/2/f2)
+    A <- ifelse(f1 > 0, n * f0.hat/(n * f0.hat + f1), 1)
+    Chat <- round(1 - f1/n * A, 4)
+    c(n, Sobs, Chat, fk)
+  }
+  Fun.ince <- function(x) {
+    nT <- x[1]
+    x <- x[-1]
+    U <- sum(x)
+    Qk <- sapply(1:10, function(k) sum(x == k))
+    Q1 <- Qk[1]
+    Q2 <- Qk[2]
+    Sobs <- sum(x > 0)
+    Q0.hat <- ifelse(Q2 == 0, (nT - 1)/nT * Q1 * (Q1 - 1)/2,
+                     (nT - 1)/nT * Q1^2/2/Q2)
+    A <- ifelse(Q1 > 0, nT * Q0.hat/(nT * Q0.hat + Q1),
+                1)
+    Chat <- round(1 - Q1/U * A, 4)
+    out <- c(nT, U, Sobs, Chat, Qk)
+  }
+  if (datatype == "abundance") {
+    if (class(x) == "numeric" | class(x) == "integer") {
+      out <- matrix(Fun.abun(x), nrow = 1)
+    }
+    else if (class(x) == "list") {
+      out <- do.call("rbind", lapply(x, Fun.abun))
+    }
+    else if (class(x) == "matrix" | class(x) == "data.frame") {
+      out <- t(apply(as.matrix(x), 2, Fun.abun))
+    }
+    if (nrow(out) > 1) {
+      out <- data.frame(site = rownames(out), out)
+      colnames(out) <- c("site", "n", "S.obs", "SC", paste("f",
+                                                           1:10, sep = ""))
+      rownames(out) <- NULL
+    }
+    else {
+      out <- data.frame(site = "site.1", out)
+      colnames(out) <- c("site", "n", "S.obs", "SC", paste("f",
+                                                           1:10, sep = ""))
+    }
+    as.data.frame(out)
+  }
+  else if (datatype == "incidence") {
+    if (class(x) == "numeric" | class(x) == "integer") {
+      out <- matrix(Fun.ince(x), nrow = 1)
+    }
+    else if (class(x) == "list") {
+      out <- do.call("rbind", lapply(x, Fun.ince))
+    }
+    else if (class(x) == "matrix" | class(x) == "data.frame") {
+      out <- t(apply(as.matrix(x), 2, Fun.ince))
+    }
+    if (nrow(out) > 1) {
+      out <- data.frame(site = rownames(out), out)
+      colnames(out) <- c("site", "T", "U", "S.obs", "SC",
+                         paste("Q", 1:10, sep = ""))
+      rownames(out) <- NULL
+    }
+    else {
+      out <- data.frame(site = "site.1", out)
+      colnames(out) <- c("site", "T", "U", "S.obs", "SC",
+                         paste("Q", 1:10, sep = ""))
+    }
+    as.data.frame(out)
+  }
+}
+
+
+
+ggiNEXT = function (x, type = 1, se = TRUE, facet.var = "none", color.var = "site",
+                    grey = FALSE) {
+  UseMethod("ggiNEXT", x)
+}
+
+ggiNEXT.iNEXT = function (x, type = 1, se = TRUE, facet.var = "none", color.var = "site",
+                          grey = FALSE) {
+  TYPE <- c(1, 2, 3)
+  SPLIT <- c("none", "order", "site", "both")
+  if (is.na(pmatch(type, TYPE)) | pmatch(type, TYPE) == -1)
+    stop("invalid plot type")
+  if (is.na(pmatch(facet.var, SPLIT)) | pmatch(facet.var,
+                                               SPLIT) == -1)
+    stop("invalid facet variable")
+  if (is.na(pmatch(color.var, SPLIT)) | pmatch(color.var,
+                                               SPLIT) == -1)
+    stop("invalid color variable")
+  type <- pmatch(type, 1:3)
+  facet.var <- match.arg(facet.var, SPLIT)
+  color.var <- match.arg(color.var, SPLIT)
+  if (facet.var == "order")
+    color.var <- "site"
+  if (facet.var == "site")
+    color.var <- "order"
+  options(warn = -1)
+  z <- fortify(x, type = type)
+  options(warn = 0)
+  if (ncol(z) == 7) {
+    se <- FALSE
+  }
+  datatype <- unique(z$datatype)
+  if (color.var == "none") {
+    if (levels(factor(z$order)) > 1 & "site" %in% names(z)) {
+      warning("invalid color.var setting, the iNEXT object consists multiple sites and orders, change setting as both")
+      color.var <- "both"
+      z$col <- z$shape <- paste(z$site, z$order, sep = "-")
+    }
+    else if ("site" %in% names(z)) {
+      warning("invalid color.var setting, the iNEXT object consists multiple orders, change setting as order")
+      color.var <- "site"
+      z$col <- z$shape <- z$site
+    }
+    else if (levels(factor(z$order)) > 1) {
+      warning("invalid color.var setting, the iNEXT object consists multiple sites, change setting as site")
+      color.var <- "order"
+      z$col <- z$shape <- factor(z$order)
+    }
+    else {
+      z$col <- z$shape <- rep(1, nrow(z))
+    }
+  }
+  else if (color.var == "order") {
+    z$col <- z$shape <- factor(z$order)
+  }
+  else if (color.var == "site") {
+    if (!"site" %in% names(z)) {
+      warning("invalid color.var setting, the iNEXT object do not consist multiple sites, change setting as order")
+      z$col <- z$shape <- factor(z$order)
+    }
+    z$col <- z$shape <- z$site
+  }
+  else if (color.var == "both") {
+    if (!"site" %in% names(z)) {
+      warning("invalid color.var setting, the iNEXT object do not consist multiple sites, change setting as order")
+      z$col <- z$shape <- factor(z$order)
+    }
+    z$col <- z$shape <- paste(z$site, z$order, sep = "-")
+  }
+  zz = z
+  z$method[z$method == "observed"] = "interpolated"
+  z$lty <- z$lty <- factor(z$method, levels = unique(c("interpolated",
+                                                       "extrapolated"), c("interpolation", "interpolation",
+                                                                          "extrapolation")))
+  z$col <- factor(z$col)
+  data.sub <- zz[which(zz$method == "observed"), ]
+  g <- ggplot(z, aes_string(x = "x", y = "y", colour = "col")) +
+    geom_point(aes_string(shape = "shape"), size = 5, data = data.sub)
+  g <- g + geom_line(aes_string(linetype = "lty"), lwd = 1.5) +
+    guides(linetype = guide_legend(title = "Method"), colour = guide_legend(title = "Guides"),
+           fill = guide_legend(title = "Guides"), shape = guide_legend(title = "Guides")) +
+    theme(legend.position = "bottom", legend.title = element_blank(),
+          text = element_text(size = 18), legend.key.width = unit(1.2,
+                                                                  "cm"))
+  if (type == 2L) {
+    g <- g + labs(x = "Number of sampling units", y = "Sample coverage")
+    if (datatype == "abundance")
+      g <- g + labs(x = "Number of individuals", y = "Sample coverage")
+  }
+  else if (type == 3L) {
+    g <- g + labs(x = "Sample coverage", y = "Species diversity")
+  }
+  else {
+    g <- g + labs(x = "Number of sampling units", y = "Species diversity")
+    if (datatype == "abundance")
+      g <- g + labs(x = "Number of individuals", y = "Species diversity")
+  }
+  if (se)
+    g <- g + geom_ribbon(aes_string(ymin = "y.lwr", ymax = "y.upr",
+                                    fill = "factor(col)", colour = "NULL"), alpha = 0.2)
+  if (facet.var == "order") {
+    if (length(levels(factor(z$order))) == 1 & type != 2) {
+      warning("invalid facet.var setting, the iNEXT object do not consist multiple orders.")
+    }
+    else {
+      g <- g + facet_wrap(~order, nrow = 1)
+      if (color.var == "both") {
+        g <- g + guides(colour = guide_legend(title = "Guides",
+                                              ncol = length(levels(factor(z$order))), byrow = TRUE),
+                        fill = guide_legend(title = "Guides"))
+      }
+    }
+  }
+  if (facet.var == "site") {
+    if (!"site" %in% names(z)) {
+      warning("invalid facet.var setting, the iNEXT object do not consist multiple sites.")
+    }
+    else {
+      g <- g + facet_wrap(~site, nrow = 1)
+      if (color.var == "both") {
+        g <- g + guides(colour = guide_legend(title = "Guides",
+                                              nrow = length(levels(factor(z$order)))), fill = guide_legend(title = "Guides"))
+      }
+    }
+  }
+  if (facet.var == "both") {
+    if (length(levels(factor(z$order))) == 1 | !"site" %in%
+        names(z)) {
+      warning("invalid facet.var setting, the iNEXT object do not consist multiple sites or orders.")
+    }
+    else {
+      g <- g + facet_wrap(site ~ order)
+      if (color.var == "both") {
+        g <- g + guides(colour = guide_legend(title = "Guides",
+                                              nrow = length(levels(factor(z$site))), byrow = TRUE),
+                        fill = guide_legend(title = "Guides"))
+      }
+    }
+  }
+  if (grey) {
+    g <- g + theme_bw(base_size = 18) + scale_fill_grey(start = 0,
+                                                        end = 0.4) + scale_colour_grey(start = 0.2, end = 0.2) +
+      guides(linetype = guide_legend(title = "Method"),
+             colour = guide_legend(title = "Guides"), fill = guide_legend(title = "Guides"),
+             shape = guide_legend(title = "Guides")) + theme(legend.position = "bottom",
+                                                             legend.title = element_blank())
+  }
+  g <- g + theme(legend.box = "vertical")
+  return(g)
+}
+
+ggiNEXT.default = function (x, ...) {
+  stop("iNEXT doesn't know how to deal with data of class ",
+       paste(class(x), collapse = "/"), call. = FALSE)
+}
 
 DataInfo = function (x, datatype = "abundance") {
   TYPE <- c("abundance", "incidence", "incidence_freq", "incidence_raw")
@@ -532,4 +779,178 @@ DataInfo = function (x, datatype = "abundance") {
     }
     as.data.frame(out)
   }
+}
+
+as.abucount = function (x) {
+  if (class(x) == "data.frame" | class(x) == "matrix") {
+    y <- rowSums(x)
+    y
+  }
+  else if (class(x) == "numeric" | class(x) == "integer" |
+           class(x) == "double") {
+    warnings("Ambiguous data type, the input object is a vector. Set total number of sampling units as 1.")
+    x
+  }
+  else {
+    stop("invalid data type, it should be a data.frame or matrix.")
+  }
+}
+
+as.incfreq = function (x) {
+  if (class(x) == "data.frame" | class(x) == "matrix") {
+    a <- sort(as.numeric(unique(c(unlist(x)))))
+    if (!identical(a, c(0, 1))) {
+      warning("Invalid data type, the element of species by sites presence-absence matrix should be 0 or 1. Set nonzero elements as 1.")
+      x <- (x > 0)
+    }
+    nT <- ncol(x)
+    y <- rowSums(x)
+    y <- c(nT, y)
+    y
+  }
+  else if (class(x) == "numeric" | class(x) == "integer" |
+           class(x) == "double") {
+    warnings("Ambiguous data type, the input object is a vector. Set total number of sampling units as 1.")
+    c(1, x)
+  }
+  else {
+    stop("Invalid data type, it should be a data.frame or matrix.")
+  }
+}
+
+estimateD = function (x, datatype = "abundance", base = "size", level = NULL,
+                      conf = 0.95) {
+  TYPE <- c("abundance", "incidence", "incidence_freq", "incidence_raw")
+  if (is.na(pmatch(datatype, TYPE)))
+    stop("invalid datatype")
+  if (pmatch(datatype, TYPE) == -1)
+    stop("ambiguous datatype")
+  datatype <- match.arg(datatype, TYPE)
+  if (datatype == "incidence") {
+    stop("datatype=\"incidence\" was no longer supported after v2.0.8, \n         please try datatype=\"incidence_freq\".")
+  }
+  if (datatype == "incidence_freq")
+    datatype <- "incidence"
+  if (datatype == "incidence_raw") {
+    if (class(x) == "data.frame" | class(x) == "matrix")
+      x <- as.incfreq(x)
+    else if (class(x) == "list")
+      x <- lapply(x, as.incfreq)
+    datatype <- "incidence"
+  }
+  BASE <- c("size", "coverage")
+  if (is.na(pmatch(base, BASE)))
+    stop("invalid datatype")
+  if (pmatch(base, BASE) == -1)
+    stop("ambiguous datatype")
+  base <- match.arg(base, BASE)
+  if (base == "size") {
+    tmp <- invSize(x, datatype, size = level, conf = conf)
+  }
+  else if (base == "coverage") {
+    tmp <- invChat(x, datatype, C = level, conf = conf)
+  }
+  tmp <- tmp[!duplicated(tmp), ]
+  nam <- names(x)
+  if (is.null(nam)) {
+    tmp
+  }
+  else if (ncol(tmp) == 6) {
+    tmp <- cbind(site = nam, tmp)
+  }
+  else {
+    tmp <- cbind(site = rep(nam, each = 3), tmp)
+  }
+  rownames(tmp) <- NULL
+  tmp
+}
+
+fortify.iNEXT = function (model, data = model$iNextEst, type = 1, ...) {
+  datatype <- ifelse(names(model$DataInfo)[2] == "n", "abundance",
+                     "incidence")
+  z <- data
+  if (class(z) == "list") {
+    z <- data.frame(do.call("rbind", z), site = rep(names(z),
+                                                    sapply(z, nrow)))
+    rownames(z) <- NULL
+  }
+  else {
+    z$site <- ""
+  }
+  if (ncol(z) == 6) {
+    warning("invalid se setting, the iNEXT object do not consist confidence interval")
+    se <- FALSE
+  }
+  else if (ncol(z) > 6) {
+    se <- TRUE
+  }
+  if (type == 1L) {
+    z$x <- z[, 1]
+    z$y <- z$qD
+    if (se) {
+      z$y.lwr <- z[, 5]
+      z$y.upr <- z[, 6]
+    }
+  }
+  else if (type == 2L) {
+    if (length(unique(z$order)) > 1) {
+      z <- subset(z, order == unique(z$order)[1])
+    }
+    z$x <- z[, 1]
+    z$y <- z$SC
+    if (se) {
+      z$y.lwr <- z[, 8]
+      z$y.upr <- z[, 9]
+    }
+  }
+  else if (type == 3L) {
+    z$x <- z$SC
+    z$y <- z$qD
+    if (se) {
+      z$y.lwr <- z[, 5]
+      z$y.upr <- z[, 6]
+    }
+  }
+  z$datatype <- datatype
+  z$plottype <- type
+  if (se) {
+    data <- z[, c("datatype", "plottype", "site", "method",
+                  "order", "x", "y", "y.lwr", "y.upr")]
+  }
+  else {
+    data <- z[, c("datatype", "plottype", "site", "method",
+                  "order", "x", "y")]
+  }
+  data
+}
+
+print.iNEXT = function (x, ...) {
+  site.n <- nrow(x$DataInfo)
+  order.n <- ifelse(site.n > 1, paste(unique(x$iNextEst[[1]]$order),
+                                      collapse = ", "), paste(unique(x$iNextEst$order), collapse = ", "))
+  cat("Compare ", site.n, " assemblages with Hill number order q = ",
+      order.n, ".\n", sep = "")
+  cat("$class: iNEXT\n\n")
+  cat("$DataInfo: basic data information\n")
+  print(x$DataInfo)
+  cat("\n")
+  cat("$iNextEst: diversity estimates with rarefied and extrapolated samples.\n")
+  if (class(x$iNextEst) == "data.frame") {
+    y <- x$iNextEst
+    m <- quantile(y[, 1], type = 1)
+    res <- y[y[, 1] %in% m, ]
+  }
+  else {
+    res <- lapply((x$iNextEst), function(y) {
+      m <- quantile(y[, 1], type = 1)
+      y[y[, 1] %in% m, ]
+    })
+  }
+  print(res)
+  cat("\n")
+  cat("$AsyEst: asymptotic diversity estimates along with related statistics.\n")
+  print(x$AsyEst)
+  cat("\n")
+  cat("NOTE: Only show five estimates, call iNEXT.object$iNextEst. to show complete output.\n")
+  return(invisible())
 }
